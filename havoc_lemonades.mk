868d374657663216f0e1bf74649fd3440d66ae4b
chandu078 <chandudyavanapelli03@gmail.com>
2021-09-22 08:37:11 +0000

    lemonades: Commonize maintainer info
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- havoc_lemonades.mk
+++ havoc_lemonades.mk
@@ -29,8 +29,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Official
 HAVOC_BUILD_TYPE := Official
-HAVOC_MAINTAINER := Chandu
-HAVOC_GROUP_URL := https://t.me/havoc_oneplus8
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := havoc_lemonades
